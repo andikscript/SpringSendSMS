469ba4b240f523eca1cd91f7fab8e1dea371c66f
andikscript <andikscript@gmail.com>
2022-08-10 15:47:38 +0700

    initial commit

---
+++ src/main/java/com/example/springsendsms/SpringSendSmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsendsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSendSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSendSmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springsendsms/SpringSendSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsendsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSendSmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
