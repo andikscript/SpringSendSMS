1493e00fdfd6a94107f86b79796864cc7ce7c509
andikscript <andikscript@gmail.com>
2022-08-10 20:34:05 +0700

    refactor variable for remove

--- src/main/java/com/example/springsendsms/model/MessagePhone.java
+++ src/main/java/com/example/springsendsms/model/MessagePhone.java
@@ -1,34 +1,7 @@
 package com.example.springsendsms.model;
 
 public class MessagePhone {
-
-    private String toPhone;
-
-    private String fromPhone;
-
     private String message;
-
-    public MessagePhone(String toPhone, String fromPhone, String message) {
-        this.toPhone = toPhone;
-        this.fromPhone = fromPhone;
-        this.message = message;
-    }
-
-    public String getToPhone() {
-        return toPhone;
-    }
-
-    public void setToPhone(String toPhone) {
-        this.toPhone = toPhone;
-    }
-
-    public String getFromPhone() {
-        return fromPhone;
-    }
-
-    public void setFromPhone(String fromPhone) {
-        this.fromPhone = fromPhone;
-    }
 
     public String getMessage() {
         return message;
