d11c20514f0e2c6445a53ed8eff3427892d5a7cb
andikscript <andikscript@gmail.com>
2022-08-24 09:30:14 +0700

    add queue

--- src/main/java/com/example/springsendsms/controller/PhoneController.java
+++ src/main/java/com/example/springsendsms/controller/PhoneController.java
@@ -1,7 +1,9 @@
 package com.example.springsendsms.controller;
 
 import com.example.springsendsms.model.MessagePhone;
+import com.example.springsendsms.model.MessageProcess;
 import com.example.springsendsms.service.MessageService;
+import com.github.sonus21.rqueue.core.RqueueMessageEnqueuer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +20,8 @@ public class PhoneController {
 
     @GetMapping(value = "/{phone}/verification")
     public ResponseEntity<?> verificationPhone(@PathVariable(value = "phone") String phone,
-                                               @RequestBody MessagePhone message) {
-        messageService.sendMessage(phone, message);
+                                               @RequestBody MessagePhone messagePhone) {
+        messageService.sendMessage(phone, messagePhone);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("Code Sent Success");
